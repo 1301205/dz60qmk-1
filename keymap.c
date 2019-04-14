ac19fa5eedf5bd9d16bb8e914a189e087ecea375
1301205 <49589402+1301205@users.noreply.github.com>
2019-04-14 15:18:16 +0200

    Update keymap.c

--- keymap.c
+++ keymap.c
@@ -1,38 +1,44 @@
 #include QMK_KEYBOARD_H
 
-#define ______ KC_TRNS
+// LAYERS
+#define _QWERTY 0      // QWERTY layer
+#define _QW _QWERTY
+
+#define _FUNCTION 1    // Function layer
+#define _FN _FUNCTION
+
+#define _CONTROL 2     // Control layer
+#define _CN _CONTROL
+
+#define MO_FN MO(1)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
- 
+  [_QWERTY] = LAYOUT_60_iso(
+    //       2        3        4        5        6        7        8        9        10       11       12       13       14       15       16
+    KC_GESC, KC_0,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_GRV,  KC_SLSH, KC_EQL, KC_BSPC, \
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, \
+    KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_BSLS, KC_ENT, \
+    KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_MINS, KC_RSFT, KC_UP, KC_PSCR, \
+    KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,  KC_RALT, MO(_FN), KC_LEFT, KC_DOWN, KC_RIGHT \
+  ),
 
-  [_BL] = LAYOUT_directional(
-      KC_GESC, KC_0,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_GRV,  KC_SLSH, KC_EQL, KC_BSPC, 
-      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,
-      MO(1),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_BSLS, KC_ENT,
-      KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_MINS, KC_RSFT, KC_UP, KC_PSCR,
-      KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,  KC_SPC,  KC_SPC,  KC_RALT, MO(_FL), KC_LEFT, KC_DOWN, KC_RIGHT
-      ),
+  [_FUNCTION] = LAYOUT_60_iso(
+    //       2        3        4        5        6        7        8        9        10       11       12       13       14       15       16
+    KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
+    _______, KC_CALC, KC_UP,   _______, _______, _______, KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP, KC_PSCR, KC_PAUS,
+    _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, KC_DEL,  KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______, _______, _______,
+    _______, _______, _______, KC_APP,  _______, _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______,
+    _______, _______, _______,                   TG(_CN),                            _______, _______, _______, _______
+  ),
 
-  /* FN Layer
-   * ,-----------------------------------------------------------------------------------------.
-   * |  `  |  F1 |  F2 |  F3 |  F4 |  F5 |  F6 |  F7 |  F8 |  F9 | F10 | F11 | F12 |    DEL    |
-   * |-----------------------------------------------------------------------------------------+
-   * |        |RBB T|RGB M| Hue+| Hue-| Sat+| Sat-| Val+| Val-|     |     |      |     |       |
-   * |-----------------------------------------------------------------------------------------+
-   * |         | BL T| BL M| BL+ | BL- |     |     |     |     |     |     |     |             |
-   * |-----------------------------------------------------------------------------------------+
-   * |RESET|     |     |     |     |     |     |     |     |     |     |     |     |     |     |
-   * |-----------------------------------------------------------------------------------------+
-   * |      |       |       |                                   |     |      |     |     |     |
-   * `-----------------------------------------------------------------------------------------'
-   */
+  [_CONTROL] = LAYOUT_60_iso(
+    //       2        3        4        5        6        7        8        9        10       11       12       13       14       15       16
+    TG(_CN), _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET,
+    _______, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, RGB_TOG, RGB_HUD, RGB_SAD, RGB_VAD, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, BL_DEC,  BL_TOGG, BL_INC,  BL_STEP, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______,                   XXXXXXX,                            _______, _______, _______, _______
+  ),
 
-   [_FL] = LAYOUT_directional(
-      KC_GRV, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, KC_F11, KC_F12, KC_TRNS, KC_DEL,
-      ______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, ______, ______, ______, ______, ______,
-      ______, BL_TOGG, BL_STEP, BL_INC, BL_DEC, ______, ______, ______, ______, ______, ______, ______,
-      RESET, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______,
-      ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______
-      ),
 };
